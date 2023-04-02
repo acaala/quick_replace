b35641e30f639604fb457864363b7fe4c80a1e0d
Liam Watts <elnewatts@gmail.com>
2023-04-03 00:17:37 +0100

    Refactored code. Added backup file and flag.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,36 @@
 use std::borrow::Cow;
-use std::fs::File;
-use std::process;
+use std::fs::{File, self};
+use std::{process, path};
 use std::time::Instant;
+use std::path::Path;
+
 use std::io::{prelude::*, self};
 
-pub fn run(from: &String, to: &String, file_path: &String, new_path: &String) -> io::Result<()> {
+use clap::{Arg, Args};
+
+pub fn run(config: Config) -> io::Result<()> {
     let start = Instant::now();
    
-    let mut file = File::open(&file_path)?;
+    let mut file = File::open(&config.file_path)?;
     let mut buf = vec![];
     file.read_to_end(&mut buf)?;
     let contents = String::from_utf8_lossy(&buf);
 
-    let result = find_matches_and_replace(contents, from, to);
 
-    create_file_and_put_contents(result, new_path)?;
+    if find_matches(&contents, &config.from) == 0 {
+        println!("Exiting...");
+        process::exit(0)
+    };
+
+    if !config.replace {
+        println!("Saving backup file..");
+        create_backup(&config.file_path)?;
+    }
+
+
+    let result = replace(contents, &config.from, &config.to);
+
+    create_file_and_put_contents(result, &config.file_path)?;
 
     let duration = start.elapsed();
     println!("Done in: {:#?}", duration);
@@ -22,17 +38,29 @@ pub fn run(from: &String, to: &String, file_path: &String, new_path: &String) ->
     Ok(())
 }
 
-fn find_matches_and_replace(contents: Cow<str>, from: &String, to: &String) -> String {
+
+fn find_matches(contents: &Cow<str>, from: &String) -> usize {
     let string_matches = contents.matches(&*from).into_iter().count();
     println!("Found {:#?} instances of {:#?}", string_matches, from);
-    
-    if string_matches == 0 {
-        println!("Exiting...");
-        process::exit(0)
-    }
 
-    println!("Replacing to {:?}..", to);
+    string_matches
+}
+
+fn replace(contents: Cow<str>, from: &String, to: &String) -> String {
+
+    println!("Replacing {:?} to {:?}..", from, to);
     contents.replace(&*from, &to)
+}
+
+fn create_backup(file_path: &String) -> io::Result<()> {
+   let original_file_path = Path::new(&file_path);
+
+    let backup_file_name = file_path.to_owned() + ".bak";
+    let backup_path = Path::new(&backup_file_name);
+    
+    fs::rename(&original_file_path, &backup_path)?;
+
+    Ok(())
 }
 
 fn create_file_and_put_contents(content_to_write: String, new_path: &String) -> io::Result<()> {
@@ -41,4 +69,17 @@ fn create_file_and_put_contents(content_to_write: String, new_path: &String) -> 
     new_file.write_all(content_to_write.as_bytes())?;
 
     Ok(())
+}
+
+pub struct Config {
+    from: String,
+    to: String,
+    file_path: String,
+    replace: bool,
+}
+
+impl Config {
+    pub fn build(from: String, to: String, file_path: String, replace: bool) -> Config {
+        Config { from, to, file_path, replace }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,33 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// Text to be replaced.
     #[arg(index = 1)]
     from: String,
 
+    /// Text to replace with.
     #[arg(index = 2)]
     to: String,
 
+    /// Path to file.
     #[arg(index = 3)]
-    path: String,
+    file_path: String,
 
-    #[arg(index = 4)]
-    new_path: String,
+    /// If you dont want to create a backup file.
+    #[arg(short, long, default_value_t = false)]
+    replace: bool,
 }
 fn main() {
     let args = Args::parse();
 
-    if let Err(error) = qrep::run(&args.from, &args.to, &args.path, &args.new_path) {
+    // TODO: Not this..
+    let file_path_clone = args.file_path.clone();
 
+    let config = qrep::Config::build(args.from, args.to, args.file_path, args.replace);
+
+    if let Err(error) = qrep::run(config) {
         match error.kind() {
-            ErrorKind::NotFound => eprintln!("{:?} does not exists.", &args.path),
+            ErrorKind::NotFound => eprintln!("{:?} does not exist.", file_path_clone),
             _ =>  eprintln!("An error has occured: {:#?}", error),
         }
        
